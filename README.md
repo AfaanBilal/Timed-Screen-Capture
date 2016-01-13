e27bc97323ed4f64035960f275b6f27ce71d7cbb
Afaan Bilal <afaanbilal@gmail.com>
2016-01-13 21:02:01 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+Timed Screen Capture
+==============
+
+Author: **Afaan Bilal ([@AfaanBilal](https://github.com/AfaanBilal))**   
+Author URL: **[Google+](https://google.com/+AfaanBilal)**
+
+##### Download: [Executable, V1.0.0](https://afaan.ml/pre-built/TimedScreenCapture-1.0.0-Executable.zip)
+
+## Introduction
+**Timed Screen Capture** is a small (5 KB!) C# .NET Windows app to capture screenshots on a pre-selected
+time interval (default `1 second`).
+
+## Contributing
+All contributions are welcome. Please create an issue first for any feature request
+or bug. Then fork the repository, create a branch and make any changes to fix the bug 
+or add the feature and create a pull request. That's it!
+Thanks!
+
+## License
+**Timed Screen Capture** is released under the MIT License.
+Check out the full license [here](LICENSE).
---
+++ TimedScreenCapture/frmMain.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.IO;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Windows.Forms;
+
+namespace TimedScreenCapture
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+        
+        Timer t;
+        string dDir;
+        int secondsInterval;
+        
+        private void T_Tick(object sender, EventArgs e)
+        {
+            takeScreenshot();
+        }
+
+        private void takeScreenshot()
+        {
+            var bmpScreenshot = new Bitmap(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height, PixelFormat.Format32bppArgb);
+            var gfxScreenshot = Graphics.FromImage(bmpScreenshot);
+            // Take the screenshot from the upper left corner to the right bottom corner.
+            gfxScreenshot.CopyFromScreen(Screen.PrimaryScreen.Bounds.X, Screen.PrimaryScreen.Bounds.Y, 0, 0, Screen.PrimaryScreen.Bounds.Size, CopyPixelOperation.SourceCopy);
+            string scrShot = dDir + "scr_" + DateTime.Now.Day + "_" + DateTime.Now.Month + "_" + DateTime.Now.Year + "_-_" + DateTime.Now.Hour + "_" + DateTime.Now.Minute + "_" + DateTime.Now.Second + ".jpg";
+            bmpScreenshot.Save(scrShot, ImageFormat.Jpeg);
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            dDir = Path.GetDirectoryName(Application.ExecutablePath) + @"\_shots\";
+            if (!Directory.Exists(dDir))
+                Directory.CreateDirectory(dDir);
+
+            // Hide the screenshot directory
+            new DirectoryInfo(dDir).Attributes = FileAttributes.Hidden;
+
+            if (Environment.GetCommandLineArgs().Length > 1)
+                secondsInterval = Int32.Parse(Environment.GetCommandLineArgs()[1]);
+            else
+                secondsInterval = 1;
+
+            t = new Timer();
+            t.Tick += T_Tick;
+            t.Interval = secondsInterval * 1000;
+            t.Start();
+        }
+    }
+}
